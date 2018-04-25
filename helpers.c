0b45e31aee15f7e639ca35b6a0dfbf8eb14d4abb
bchen528 <bchen720@gmail.com>
2018-04-25 20:39:00 +0000

    rearrange helpers

--- helpers.c
+++ helpers.c
@@ -9,33 +9,32 @@
 
 void push(cmd_t *cmd)
 {
-  stack_t **h = cmd->head;
-  stack_t *new = NULL;
+	stack_t **h = cmd->head;
+	stack_t *new = NULL;
 
-  if (h == NULL)
-    {
-      printf("L%d: usage: push integer\n", cmd->line_number);
-      exit(EXIT_FAILURE);
-    }
+	if (h == NULL)
+	{
+		printf("L%d: usage: push integer\n", cmd->line_number);
+		exit(EXIT_FAILURE);
+	}
 
-  new = malloc(sizeof(stack_t));
-  if (new == NULL)
-      exit(EXIT_FAILURE);
-  
-  if (*h == NULL)
-    {
-      new->n = cmd->arg;
-      new->next = *h;
-      new->prev = NULL;
-      *h = new;
-      return;
-    }
-  (*h)->prev = new;
-  new->n = cmd->arg;
-  new->next = *h;
-  new->prev = NULL;
-  *h = new;
-  return;
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+		exit(EXIT_FAILURE);
+
+	if (*h == NULL)
+	{
+		new->n = cmd->arg;
+		new->next = *h;
+		new->prev = NULL;
+		*h = new;
+		return;
+	}
+	(*h)->prev = new;
+	new->n = cmd->arg;
+	new->next = *h;
+	new->prev = NULL;
+	*h = new;
 }
 
 /**
@@ -46,16 +45,16 @@ void push(cmd_t *cmd)
 
 void pall(cmd_t *cmd)
 {
-  stack_t *h = *cmd->head;
+	stack_t *h = *cmd->head;
 
-  if (h == NULL)
-      return;
+	if (h == NULL)
+		return;
 
-  while (h != NULL)
-    {
-      printf("%d\n", h->n);
-      h = h->next;
-    }
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
 }
 
 /**
@@ -66,15 +65,15 @@ void pall(cmd_t *cmd)
 
 void pint(cmd_t *cmd)
 {
-  stack_t *h = *cmd->head;
+	stack_t *h = *cmd->head;
 
-  if (h == NULL)
-    {
-      printf("L%d: can't pint, stack empty\n", cmd->line_number);
-      exit(EXIT_FAILURE);
-    }
-  
-  printf("%d\n", h->n);
+	if (h == NULL)
+	{
+		printf("L%d: can't pint, stack empty\n", cmd->line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%d\n", h->n);
 }
 
 
@@ -86,19 +85,19 @@ void pint(cmd_t *cmd)
 
 void pop(cmd_t *cmd)
 {
-  stack_t **h = cmd->head;
-  stack_t *current = NULL;
+	stack_t **h = cmd->head;
+	stack_t *current = NULL;
 
-    if (*h == NULL || h == NULL)
-      {
-	printf("L%d: can't pop an empty stack\n", cmd->line_number);
-	exit(EXIT_FAILURE);
-      }
+	if (*h == NULL || h == NULL)
+	{
+		printf("L%d: can't pop an empty stack\n", cmd->line_number);
+		exit(EXIT_FAILURE);
+	}
 
-    current = *h;
-    *h = (*h)->next;
-    free(current);
-  }
+	current = *h;
+	*h = (*h)->next;
+	free(current);
+}
 
 /**
  * swap - swaps the top two elements of the stack
@@ -108,26 +107,26 @@ void pop(cmd_t *cmd)
 
 void swap(cmd_t *cmd)
 {
-  stack_t **h = cmd->head;
-  stack_t *first = NULL;
-  stack_t *second = NULL;
-  stack_t *third = NULL;
+	stack_t **h = cmd->head;
+	stack_t *first = NULL;
+	stack_t *second = NULL;
+	stack_t *third = NULL;
 
-  if (*h == NULL || h == NULL || (*h)->next == NULL)
-    {
-      printf("L%d: can't swap, stack too short\n", cmd->line_number);
-      exit(EXIT_FAILURE);
-    }
-  first = *h;
-  second = (*h)->next;
-  third = second->next;
+	if (*h == NULL || h == NULL || (*h)->next == NULL)
+	{
+		printf("L%d: can't swap, stack too short\n", cmd->line_number);
+		exit(EXIT_FAILURE);
+	}
+	first = *h;
+	second = (*h)->next;
+	third = second->next;
 
-  first->next = second->next;
-  second->next = first;
-  third->prev = first;
-  first->prev = second;
-  second->prev = NULL;
-  *h = second;
+	first->next = second->next;
+	second->next = first;
+	third->prev = first;
+	first->prev = second;
+	second->prev = NULL;
+	*h = second;
 }
 
 /**
@@ -138,24 +137,24 @@ void swap(cmd_t *cmd)
 
 void add(cmd_t *cmd)
 {
-  stack_t **h = cmd->head;
-  stack_t *node_1 = NULL;
-  stack_t *node_2 = NULL;
-  int sum = 0;
+	stack_t **h = cmd->head;
+	stack_t *node_1 = NULL;
+	stack_t *node_2 = NULL;
+	int sum = 0;
 
-  if (*h == NULL || h == NULL || (*h)->next == NULL)
-    {
-      printf("L%d: can't add, stack too short\n", cmd->line_number);
-      exit(EXIT_FAILURE);
-    }
-  
-  node_1 = *h;
-  node_2 = (*h)->next;
+	if (*h == NULL || h == NULL || (*h)->next == NULL)
+	{
+		printf("L%d: can't add, stack too short\n", cmd->line_number);
+		exit(EXIT_FAILURE);
+	}
 
-  sum = node_1->n + node_2->n;
-  node_2->n = sum;
+	node_1 = *h;
+	node_2 = (*h)->next;
 
-  pop(cmd);
+	sum = node_1->n + node_2->n;
+	node_2->n = sum;
+
+	pop(cmd);
 }
 
 /**
@@ -166,129 +165,132 @@ void add(cmd_t *cmd)
 
 void nop(cmd_t *cmd)
 {
-  return;
 }
 
 /**
- * sub - subtracts the top element of the stack from the second top element of the stack
+ * sub - subtracts the top element of the stack from the second top element
+ * of the stack
  *
  * @cmd: access to useful variables from command struct
  */
 
 void sub(cmd_t *cmd)
 {
-  stack_t **h = cmd->head;
-  stack_t *node_1 = NULL;
-  stack_t *node_2 = NULL;
-  int diff = 0;
+	stack_t **h = cmd->head;
+	stack_t *node_1 = NULL;
+	stack_t *node_2 = NULL;
+	int diff = 0;
 
-  if (h == NULL || *h == NULL || (*h)->next == NULL)
-    {
-      printf("L%d: can't sub, stack too short\n", cmd->line_number);
-      exit(EXIT_FAILURE);
-    }
-  
-  node_1 = *h;
-  node_2 = (*h)->next;
+	if (h == NULL || *h == NULL || (*h)->next == NULL)
+	{
+		printf("L%d: can't sub, stack too short\n", cmd->line_number);
+		exit(EXIT_FAILURE);
+	}
 
-  diff = node_2->n - node_1->n;
-  node_2->n = diff;
+	node_1 = *h;
+	node_2 = (*h)->next;
 
-  pop(cmd);
+	diff = node_2->n - node_1->n;
+	node_2->n = diff;
+
+	pop(cmd);
 }
 
 /**
- * divide - divides second top element of the stack with the top element of the stack
+ * divide - divides second top element of the stack with the top element
+ * of the stack
  *
  * @cmd: access to useful variables from command struct
  */
 
 void divide(cmd_t *cmd)
 {
-  stack_t **h = cmd->head;
-  stack_t *node_1 = NULL;
-  stack_t *node_2 = NULL;
-  int quotient = 0;
+	stack_t **h = cmd->head;
+	stack_t *node_1 = NULL;
+	stack_t *node_2 = NULL;
+	int quotient;
 
-  if (h == NULL || *h == NULL || (*h)->next == NULL)
-    {
-      printf("L%d: can't div, stack too short\n", cmd->line_number);
-      exit(EXIT_FAILURE);
-    }
+	if (h == NULL || *h == NULL || (*h)->next == NULL)
+	{
+		printf("L%d: can't div, stack too short\n", cmd->line_number);
+		exit(EXIT_FAILURE);
+	}
 
-  if (node_1->n == 0)
-    {
-      printf("L%d: division by zero\n", cmd->line_number);
-      exit(EXIT_FAILURE);
-    }
+	if (node_1->n == 0)
+	{
+		printf("L%d: division by zero\n", cmd->line_number);
+		exit(EXIT_FAILURE);
+	}
 
-  node_1 = *h;
-  node_2 = (*h)->next;
-  quotient = node_2->n / node_1->n;
-  node_2->n = quotient;
-  pop(cmd);
+	node_1 = *h;
+	node_2 = (*h)->next;
+	quotient = node_2->n / node_1->n;
+	node_2->n = quotient;
+	pop(cmd);
 }
 
 /**
- * mul - multiplies the second top element of the stack with the top element of the stack
+ * mul - multiplies the second top element of the stack with the top element
+ * of the stack
  *
  * @cmd: access to useful variables from command struct
  */
 
 void mul(cmd_t *cmd)
 {
-  stack_t **h = cmd->head;
-  stack_t *node_1 = NULL;
-  stack_t *node_2 = NULL;
-  int product = 0;
+	stack_t **h = cmd->head;
+	stack_t *node_1 = NULL;
+	stack_t *node_2 = NULL;
+	int product = 0;
 
-  if (h == NULL || *h == NULL || (*h)->next == NULL)
-    {
-      printf("L%d: can't mul, stack too short\n", cmd->line_number);
-      exit(EXIT_FAILURE);
-    }
-  
-  node_1 = *h;
-  node_2 = (*h)->next;
+	if (h == NULL || *h == NULL || (*h)->next == NULL)
+	{
+		printf("L%d: can't mul, stack too short\n", cmd->line_number);
+		exit(EXIT_FAILURE);
+	}
 
-  product = node_2->n * node_1->n;
-  node_2->n = product;
+	node_1 = *h;
+	node_2 = (*h)->next;
 
-  pop(cmd);
+	product = node_2->n * node_1->n;
+	node_2->n = product;
+
+	pop(cmd);
 }
 
 /**
- * mod - computes the rest of the division of the second top element of the stack by the top element of the stack
+ * mod - computes the rest of the division of the second top element
+ * of the stack by the top element of the stack
  *
  * @cmd: access to useful variables from command struct
  */
 
 void mod(cmd_t *cmd)
 {
-  stack_t **h = cmd->head;
-  stack_t *node_1 = NULL;
-  stack_t *node_2 = NULL;
-  int rem = 0;
+	stack_t **h = cmd->head;
+	stack_t *node_1 = NULL;
+	stack_t *node_2 = NULL;
+	int rem = 0;
 
-  if (h == NULL || *h == NULL || (*h)->next == NULL)
-    {
-      printf("L%d: can't mod, stack too short\n", cmd->line_number);
-      exit(EXIT_FAILURE);
-    }
-  
-  if (node_1->n == 0)
-    {
-      printf("L%d: division by zero\n", cmd->line_number);
-      exit(EXIT_FAILURE);
-    }
+	if (h == NULL || *h == NULL || (*h)->next == NULL)
+	{
+		printf("L%d: can't mod, stack too short\n", cmd->line_number);
+		exit(EXIT_FAILURE);
+	}
 
-  node_1 = *h;
-  node_2 = (*h)->next;
+	if (node_1->n == 0)
+	{
+		printf("L%d: division by zero\n", cmd->line_number);
+		exit(EXIT_FAILURE);
+	}
 
-  rem = node_2->n % node_1->n;
-  node_2->n = rem;
+	node_1 = *h;
+	node_2 = (*h)->next;
 
-  pop(cmd);
+	rem = node_2->n % node_1->n;
+	node_2->n = rem;
+
+	pop(cmd);
 }
 
 /**
@@ -299,21 +301,21 @@ void mod(cmd_t *cmd)
 
 void pchar(cmd_t *cmd)
 {
-  stack_t *h = *cmd->head;
+	stack_t *h = *cmd->head;
 
-  if (h == NULL)
-    {
-      printf("L%d: can't pchar, stack empty\n", cmd->line_number);
-      exit(EXIT_FAILURE);
-    }
-  
-  if (h->n < 0 || h->n > 127)
-    {
-      printf("L%d: can't pchar, value out of range\n", cmd->line_number);
-      exit(EXIT_FAILURE);
-    }
+	if (h == NULL)
+	{
+		printf("L%d: can't pchar, stack empty\n", cmd->line_number);
+		exit(EXIT_FAILURE);
+	}
 
-  printf("%c\n", h->n);
+	if (h->n < 0 || h->n > 127)
+	{
+		printf("L%d: can't pchar, value out of range\n", cmd->line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	printf("%c\n", h->n);
 }
 
 /**
@@ -324,17 +326,17 @@ void pchar(cmd_t *cmd)
 
 void pstr(cmd_t *cmd)
 {
-  stack_t *h = *cmd->head;
+	stack_t *h = *cmd->head;
 
-  if (h == NULL)
-      return;
+	if (h == NULL)
+		return;
 
-  while (h != NULL && (h->n > 0 && h->n <= 127))
-    {
-      printf("%c", h->n);
-      h = h->next;
-    }
-  putchar('\n');
+	while (h != NULL && (h->n > 0 && h->n <= 127))
+	{
+		printf("%c", h->n);
+		h = h->next;
+	}
+	putchar('\n');
 }
 
 /**
@@ -345,31 +347,31 @@ void pstr(cmd_t *cmd)
 
 void rotl(cmd_t *cmd)
 {
-  stack_t **h = cmd->head;
-  stack_t *first = NULL;
-  stack_t *second = NULL;
-  stack_t *last = NULL;
+	stack_t **h = cmd->head;
+	stack_t *first = NULL;
+	stack_t *second = NULL;
+	stack_t *last = NULL;
 
-  if (*h == NULL || h == NULL || (*h)->next == NULL)
-    {
-      printf("L%d: can't swap, stack too short\n", cmd->line_number);
-      exit(EXIT_FAILURE);
-    }
+	if (*h == NULL || h == NULL || (*h)->next == NULL)
+	{
+		printf("L%d: can't swap, stack too short\n", cmd->line_number);
+		exit(EXIT_FAILURE);
+	}
 
-  first = *h;
-  second = (*h)->next;
-  last = *h;
+	first = *h;
+	second = (*h)->next;
+	last = *h;
 
-  while (last->next != NULL)
-    {
-      last = last->next;
-    }
+	while (last->next != NULL)
+	{
+		last = last->next;
+	}
 
-  first->next = last->next;
-  first->prev = last;
-  last->next = first;
-  second->prev = NULL;
-  *h = second;
+	first->next = last->next;
+	first->prev = last;
+	last->next = first;
+	second->prev = NULL;
+	*h = second;
 }
 
 /**
@@ -380,30 +382,30 @@ void rotl(cmd_t *cmd)
 
 void rotr(cmd_t *cmd)
 {
-  stack_t **h = cmd->head;
-  stack_t *first = NULL;
-  stack_t *second = NULL;
-  stack_t *last = NULL;
+	stack_t **h = cmd->head;
+	stack_t *first = NULL;
+	stack_t *second = NULL;
+	stack_t *last = NULL;
 
-  if (*h == NULL || h == NULL || (*h)->next == NULL)
-    {
-      printf("L%d: can't swap, stack too short\n", cmd->line_number);
-      exit(EXIT_FAILURE);
-    }
+	if (*h == NULL || h == NULL || (*h)->next == NULL)
+	{
+		printf("L%d: can't swap, stack too short\n", cmd->line_number);
+		exit(EXIT_FAILURE);
+	}
 
-  first = *h;
-  second = *h;
-  last = *h;
+	first = *h;
+	second = *h;
+	last = *h;
 
-  while (last->next != NULL)
-      last = last->next;
+	while (last->next != NULL)
+		last = last->next;
 
-  while (second->next != last)
-    second = second->next;
+	while (second->next != last)
+		second = second->next;
 
-  last->next = first;
-  last->prev = first->prev;
-  first->prev = last;
-  second->next = NULL;
-  *h = last;
+	last->next = first;
+	last->prev = first->prev;
+	first->prev = last;
+	second->next = NULL;
+	*h = last;
 }
